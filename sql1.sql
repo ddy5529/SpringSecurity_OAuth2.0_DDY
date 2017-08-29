5ea3689eae65b589e609a62d5ca1434079a4fac6
叶星源 <1171148438@qq.com>
2017-08-29 16:57:55 +0800

    Update sql1.sql

--- sql1.sql
+++ sql1.sql
@@ -106,8 +106,7 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 INSERT INTO  oauth_client_details (
- client_id, resource_ids, client_secret, scope, authorized_grant_types, web_server_redirect_uri, authorities, access_token_validity, refresh_token_validity, additional_information, autoapprove
-)
+ `client_id`, `resource_ids`, `client_secret`, `scope`, `authorized_grant_types`, `web_server_redirect_uri`,`authorities`,`access_token_validity`, `refresh_token_validity`, `additional_information`, `autoapprove`)
 VALUES(
 'client', NULL, 'secret', 'app', 'authorization_code', 'http://www.baidu.com', NULL, NULL, NULL, NULL, NULL
 );
